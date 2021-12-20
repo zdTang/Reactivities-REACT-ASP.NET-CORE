baaefe3560b7898f6cafc1e6d74a026dedcd1866
Zhendong Tang <zhendongtt@gmail.com>
2021-12-19 21:53:41 -0500

    2-10 - Seeding data to the Database
    Seeding....
    reference Entity Framework in Action
    Pay attention to those asynchronous operation

--- API/Program.cs
+++ API/Program.cs
@@ -13,20 +13,23 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host=CreateHostBuilder(args).Build();
             using var scope=host.Services.CreateScope();
             var services = scope.ServiceProvider;
             try{
                 var context=services.GetRequiredService<DataContext>();
-                context.Database.Migrate();
+                //context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
             }catch(Exception ex){
                 var logger=services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex,"An error occurred during migration");              
             }
             
-            host.Run();
+            //host.Run();
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
