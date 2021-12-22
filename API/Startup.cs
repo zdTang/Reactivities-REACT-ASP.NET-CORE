29cc5dcf9a6eeb6cde7a25eb8c546788237cc5a4
Zhendong Tang <zhendongtt@gmail.com>
2021-12-22 12:37:06 -0500

    4-9 - Adding AutoMapper

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 
 using Application.Activities;
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -41,6 +42,7 @@ namespace API
             });
             // Inject MediatR
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
