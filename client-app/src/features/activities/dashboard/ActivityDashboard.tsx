5a0bce78ee16d7327ddfd28824bfb3511e1f95f7
Zhendong Tang <zhendongtt@gmail.com>
2021-12-24 18:30:19 -0500

    6-6 - Deleting activity on the server

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,64 +1,62 @@
 import React from "react";
-import { Grid} from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
+
 interface Props {
   activities: Activity[];
   selectedActivity: Activity | undefined;
-  selectActivity: (id: string) => void; // function type
-  cancelSelectActivity: () => void; // function type
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
   editMode: boolean;
   openForm: (id: string) => void;
   closeForm: () => void;
-  createOrEdit:(activity:Activity)=>void;
-  deleteActivity:(id:string)=>void;
-  submitting:boolean;
+  createOrEdit: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-const ActivityDashboard = ({
+export default function ActivityDashboard({
   activities,
   selectedActivity,
+  deleteActivity,
   selectActivity,
   cancelSelectActivity,
   editMode,
   openForm,
   closeForm,
   createOrEdit,
-  deleteActivity,
-  submitting
-}: Props) => {
+  submitting,
+}: Props) {
   return (
-    <div>
-      <Grid>
-        <Grid.Column width="10">
-          <ActivityList
-            activities={activities}
-            selectActivity={selectActivity}
-            deleteActivity={deleteActivity}
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting}
+        />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
           />
-        </Grid.Column>
-        <Grid.Column width="6">
-          {selectedActivity && !editMode && (
-            <ActivityDetails
-              activity={selectedActivity}
-              cancelSelectActivity={cancelSelectActivity}
-              openForm={openForm}
-            />
-          )}
-          {editMode && (
-            <ActivityForm
-              closeForm={closeForm}
-              activity={selectedActivity}
-              createOrEdit={createOrEdit}
-              submitting={submitting}
-            />
-          )}
-        </Grid.Column>
-      </Grid>
-    </div>
+        )}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            createOrEdit={createOrEdit}
+            submitting={submitting}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
   );
-};
-
-export default ActivityDashboard;
+}
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,18 +1,30 @@
-import React from "react";
-import { Item, Segment } from "semantic-ui-react";
+import React, { SyntheticEvent, useState } from "react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityList({
   activities,
   selectActivity,
   deleteActivity,
+  submitting,
 }: Props) {
+  const [target, setTarget] = useState("");
+
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    setTarget(e.currentTarget.name);
+    deleteActivity(id);
+  }
+
   return (
     <Segment>
       <Item.Group divided>
@@ -28,19 +40,21 @@ export default function ActivityList({
                 </div>
               </Item.Description>
               <Item.Extra>
-                {/* <Button floated="right" content="View" color="blue"></Button> */}
-                <button
-                  style={{ float: "right", color: "red" }}
-                  onClick={() => deleteActivity(activity.id)}
-                >
-                  Delete
-                </button>
-                <button
-                  style={{ float: "right", color: "blue" }}
+                <Button
                   onClick={() => selectActivity(activity.id)}
-                >
-                  View
-                </button>
+                  floated="right"
+                  content="View"
+                  color="blue"
+                />
+                <Button
+                  name={activity.id}
+                  loading={submitting && target === activity.id}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
