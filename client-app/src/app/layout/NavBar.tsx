ce5e97f4c2ef26ea95b8a0e565565b4dec823258
Zhendong Tang <zhendongtt@gmail.com>
2021-12-24 08:30:27 -0500

    5-5 - Adding some style to the nav bar

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Button, Container, Menu } from 'semantic-ui-react';
-
+import React from "react";
+import { Container, Menu } from "semantic-ui-react";
 
 export default function NavBar() {
-    return (
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo"  />
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item name='Activities' />
-                <Menu.Item>
-                    <Button positive content='Create Activity' />
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10em" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          {/* <Button positive content="Create Activity" /> */}
+          {/* the button cause Error at the Browser */}
+          <button>Create Activity</button>
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
 }
