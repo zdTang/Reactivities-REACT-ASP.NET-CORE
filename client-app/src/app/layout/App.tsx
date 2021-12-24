22485f2d499ab9ace0038fed41b843f1c08c30b8
Zhendong Tang <zhendongtt@gmail.com>
2021-12-24 15:56:05 -0500

    5-14 - Using a GUID for the activity id

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -27,6 +28,7 @@ function App() {
   function handleCancelSelectActivity() {
     setSelectedActivity(undefined);
   }
+
   function handleFormOpen(id?: string) {
     id ? handleSelectActivity(id) : handleCancelSelectActivity();
     setEditMode(true);
@@ -35,15 +37,20 @@ function App() {
   function handleFormClose() {
     setEditMode(false);
   }
+
   function handleCreateOrEditActivity(activity: Activity) {
     activity.id
       ? setActivities([
           ...activities.filter((x) => x.id !== activity.id),
           activity,
         ])
-      : setActivities([...activities, activity]);
+      : setActivities([...activities, { ...activity, id: uuid() }]);
     setEditMode(false);
     setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter((x) => x.id !== id)]);
   }
 
   return (
@@ -59,6 +66,7 @@ function App() {
           openForm={handleFormOpen}
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
+
         />
       </Container>
     </>
