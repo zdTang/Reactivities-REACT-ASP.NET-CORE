28e74d7d249c5aab624aec79da4192a3a8eeb890
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 07:40:16 -0500

    create activity Store

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,9 @@
+import { makeObservable, observable } from "mobx";
+
+export default class ActivityStore {
+  title = "Hello from MobX";
+  
+  constructor() {
+    makeObservable(this, { title: observable });
+  }
+}
