0a6afb6b98f9b68c8fb84f9103dc0da877ea4c56
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 12:10:40 -0500

    move Create Activity and update Activity to store

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import { v4 as uuid } from "uuid";
 
 export default class ActivityStore {
   activities: Activity[] = [];
@@ -34,7 +35,7 @@ export default class ActivityStore {
   };
 
   selectActivity = (id: string) => {
-    this.selectedActivity = this.activities.find((x) => x.id === id);;
+    this.selectedActivity = this.activities.find((x) => x.id === id);
   };
 
   cancelSelectedActivity = () => {
@@ -48,5 +49,41 @@ export default class ActivityStore {
 
   closeForm = () => {
     this.editMode = false;
+  };
+  createActivity = async (activity: Activity) => {
+    this.loading = true;
+    activity.id = uuid();
+    try {
+      await agent.Activities.create(activity);
+      runInAction(() => {
+        this.activities.push(activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  };
+
+  updateActivity = async (activity: Activity) => {
+    this.loading = true;
+    try {
+      await agent.Activities.update(activity);
+      runInAction(() => {
+        this.activities=[...this.activities.filter((x) => x.id !== activity.id),activity];
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.loading = false;
+      });
+    } catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
   };
 }
