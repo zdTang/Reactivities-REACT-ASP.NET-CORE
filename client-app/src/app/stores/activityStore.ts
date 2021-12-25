998ae628c8425480900cd6dc19be8fec958697fe
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 10:02:53 -0500

    7-6 - MobX strict mode
    using different approach to avoid using "runInAction"

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -15,21 +15,21 @@ export default class ActivityStore {
   }
 
   loadActivities = async () => {
-    this.loadingInitial = true;
+    this.setLoadingInitial(true);
     try {
       const activities = await agent.Activities.list();
-      runInAction(() => {
-        activities.forEach((activity) => {
-          activity.date = activity.date.split("T")[0];
-          this.activities.push(activity);
-        });
-        this.loadingInitial = false;
+      activities.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        this.activities.push(activity);
       });
+      this.setLoadingInitial(false);
     } catch (error) {
       console.log(error);
-      runInAction(() => {
-        this.loadingInitial = false;
-      });
+      this.setLoadingInitial(false);
     }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
   };
 }
