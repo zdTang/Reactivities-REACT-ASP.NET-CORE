dc69dcddab47d0b04b6f2fefc273977877727efd
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 17:12:10 -0500

    8-3 - Adding routes.mp4
    Move "useEffect" from App to ActivityDashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,19 @@
-import React, { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
 import HomePage from "../../features/home/HomePage";
 import { Route } from "react-router-dom";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading app" />;
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        which one match the URL, will display this component
-        <Route path="/" component={HomePage} />
+        {/* which one match the URL, will display this component */}
+        {/* use "exact" key word to match strictly */}
+        <Route exact path="/" component={HomePage} />
         <Route path="/activities" component={ActivityDashboard} />
         <Route path="/createActivity" component={ActivityForm} />
         {/* <ActivityDashboard /> */}
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,30 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
-
-
 function ActivityDashboard() {
   const { activityStore } = useStore();
   const { selectedActivity, editMode } = activityStore;
 
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading app" />;
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList/>
+        <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
         {selectedActivity && !editMode && <ActivityDetails />}
-        {editMode && (
-          <ActivityForm/>
-        )}
+        {editMode && <ActivityForm />}
       </Grid.Column>
     </Grid>
   );
