0bd8d7cf4fe58a6d620c5ba3874db24b5d5f7a79
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 17:33:47 -0500

    8-4 - Adding nav links
    Notice how use NavLink to replace a Button click Event

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from "../stores/store";
 
 export default function NavBar() {
-  const { activityStore } = useStore();
+  
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} to="/" exact header>
           <img
             src="/assets/logo.png"
             alt="logo"
@@ -15,13 +15,17 @@ export default function NavBar() {
           />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item as={NavLink} to="/activities" name="Activities" />
         <Menu.Item>
           {/* <Button positive content="Create Activity" /> */}
           {/* the button cause Error at the Browser */}
-          <Button onClick={() => activityStore.openForm()} color="blue">
-            Create Activity
-          </Button>
+          <Button
+            as={NavLink}
+            to="/CreateActivity"
+            positive
+            color="blue"
+            content=" Create Activity"
+          />
         </Menu.Item>
       </Container>
     </Menu>
