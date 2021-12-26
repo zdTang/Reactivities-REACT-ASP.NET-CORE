82535dfe542e16c83f6da3bc32e516d04cd2aecf
Zhendong Tang <zhendongtt@gmail.com>
2021-12-25 19:54:36 -0500

    8-8 - Adding the edit activity route

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -17,7 +17,10 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/activities" component={ActivityDashboard} />
         <Route path="/activities/:id" component={ActivityDetails} />
-        <Route path="/createActivity" component={ActivityForm} />
+        <Route
+          path={["/createActivity", "/manage/:id"]}
+          component={ActivityForm}
+        />
         {/* <ActivityDashboard /> */}
       </Container>
     </>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,21 @@
 import { observer } from "mobx-react-lite";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 
 function ActivityForm() {
   const { activityStore } = useStore();
-  const { selectedActivity, loading, createActivity, updateActivity } =
-    activityStore;
-  const initialState = selectedActivity ?? {
+  const {
+    loading,
+    createActivity,
+    updateActivity,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
+  const { id } = useParams<{ id: string }>();
+  const [activity, setActivity] = useState({
     id: "",
     title: "",
     category: "",
@@ -15,9 +23,11 @@ function ActivityForm() {
     date: "",
     city: "",
     venue: "",
-  };
+  });
 
-  const [activity, setActivity] = useState(initialState);
+  useEffect(() => {
+    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+  }, [id, loadActivity]);
 
   function handleSubmit() {
     activity.id ? updateActivity(activity) : createActivity(activity);
@@ -29,7 +39,7 @@ function ActivityForm() {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value }); // use new property to cover old value
   }
-
+  if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete="off">
