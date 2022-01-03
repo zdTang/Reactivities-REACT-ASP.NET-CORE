afa3ff09d4eb69cfba0b965798156238ba3def31
Zhendong Tang <zhendongtt@gmail.com>
2022-01-02 21:22:07 -0500

    using fluent validation - validate one field

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,7 @@
 
 using API.Extensions;
+using Application.Activities;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +24,11 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(Config =>
+            {
+                Config.RegisterValidatorsFromAssemblyContaining<Create>(); // tell him which Assembly is it.
+            });
+
             services.AddApplicationServices(_config);
         }
 
