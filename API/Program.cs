6434bcffb1d279142359cd1fe9f4a5147d4e1bdb
Zhendong Tang <zhendongtt@gmail.com>
2022-01-05 19:11:09 -0500

    12-5 - Adding seed users

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,10 +21,11 @@ namespace API
             using var scope=host.Services.CreateScope();
             var services = scope.ServiceProvider;
             try{
-                var context=services.GetRequiredService<DataContext>();
+                var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 //context.Database.Migrate();
-                await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await context.Database.MigrateAsync();      // Do migration 
+                await Seed.SeedData(context, userManager);  // Seeding as well 
             }catch(Exception ex){
                 var logger=services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex,"An error occurred during migration");              
